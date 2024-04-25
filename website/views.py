cfeb5b0b42a007ae36f3e870401497ea8d053768
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2024-04-25 11:05:20 +0330

    created html files
    added templates folder and html files to website and render to views

--- website/views.py
+++ website/views.py
@@ -3,11 +3,10 @@ from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
 def index_view(request):
-    return render('<h1> This is a test </h1>')
-
+    return render(request,'website/index.html')
 
 def contact_view(request):
-    return render({'name':'reihane'})
+    return render(request,'website/contact.html')
 
 def about_view(request):
-    return render({'name':'reihane'})
+    return render(request,'website/about.html')
