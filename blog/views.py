2041f5b021bbf6146e6007babf10cb9ae98ac907
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2024-07-08 11:22:07 +0330

    classifying based on author

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(status = True)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None :  
+        posts = posts.filter(author__username=kwargs['author_username'])    
     content = {'posts': posts}
     return render(request,'blog/blog-home.html',content)
 
@@ -14,3 +18,9 @@ def blog_single(request,pid):
 
 def test(request):
     return render(request,'test.html')
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request,'blog/blog-home.html',context) 
