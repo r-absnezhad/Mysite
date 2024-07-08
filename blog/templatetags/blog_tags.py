91be14a028e96f20a85f5d7028d59c544e5ba9f4
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2024-07-08 09:56:11 +0330

    category with template tags
    placing category field with template tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post,Category
 register = template.Library()
 
 @register.simple_tag (name='totalposts')
@@ -22,3 +22,13 @@ def snippet(value,args =15):
 def latestposts():
     posts = Post.objects.filter(status=1).order_by('published_date')[:2]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    category = Category.objects.all()
+    cat_dict = {}
+    for name in category:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories':cat_dict}
