aeb09ede913490a9e1841870ab46dcf752ac8d04
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2024-07-15 17:21:38 +0330

    contact version1 with forms

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from website.models import Contact
+from website.forms import NameForm
 from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
@@ -10,3 +12,18 @@ def contact_view(request):
 
 def about_view(request):
     return render(request,'website/about.html')
+
+def test_view(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            print(name, email, subject, message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse(' not done ')
+    form = NameForm()    
+    return render(request,'test.html',{'form':form})
