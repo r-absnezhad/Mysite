ce2935cf37a7de53eb074d03607c9eb880d63ce4
r-absnezhad <167188241+r-absnezhad@users.noreply.github.com>
2024-07-18 12:00:29 +0330

    ContactForm in Project
    creating ContactForm and a new model named Newsletter and it's Form and placing them in project

--- website/models.py
+++ website/models.py
@@ -15,3 +15,12 @@ class Contact(models.Model):
         
     def __str__(self):
         return self.name
+    
+
+class Newsletter(models.Model):
+    email = models.EmailField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.email    
--- website/views.py
+++ website/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
 from website.models import Contact
-from website.forms import NameForm,ContactForm
-from django.http import HttpResponse,JsonResponse
+from website.forms import NameForm,ContactForm,NewsletterForm
+from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
+from django.contrib import messages
 # Create your views here.
 
 def index_view(request):
     return render(request,'website/index.html')
 
 def contact_view(request):
-    return render(request,'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'YOU DID IT CORRECTLY')
+        else:
+            messages.add_message(request,messages.ERROR,"YOU DIDN'T DO IT CORRECTLY")
+    form = ContactForm()        
+    return render(request,'website/contact.html',{'form':form})
 
 def about_view(request):
     return render(request,'website/about.html')
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/')
+
+
+
 
 def test_view(request):
     if request.method == 'POST':
